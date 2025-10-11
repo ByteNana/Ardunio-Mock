32fc67cdcb44bcb21d1e72c5d213426560e6f1e7
Rafael de Conde Reis <82062045+Rafael-Conde@users.noreply.github.com>
2025-10-11 18:33:01 -0300

    Add `configASSERT` macro to `FreeRTOSConfig.h`
    Signed-off-by: Rafael de Conde Reis <82062045+Rafael-Conde@users.noreply.github.com>

--- src/freertos/FreeRTOSConfig.h
+++ src/freertos/FreeRTOSConfig.h
@@ -49,3 +49,17 @@
 #define INCLUDE_xTimerGetTimerDaemonTaskHandle 1
 #define INCLUDE_xTaskGetIdleTaskHandle 1
 #define INCLUDE_pcTaskGetTaskName 1
+ 
+/* It is a good idea to define configASSERT() while developing.  configASSERT()
+uses the same semantics as the standard C assert() macro. */
+// Ensure C linkage for assert hook when included in C++
+#ifdef __cplusplus
+extern "C" {
+#endif
+void vAssertCalled(unsigned long ulLine, const char* const pcFileName);
+#ifdef __cplusplus
+}
+#endif
+#define configASSERT(x) \
+  if ((x) == 0) vAssertCalled(__LINE__, __FILE__)
+
